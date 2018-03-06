cbec3d396f33f4a186c373274c53af49928b61b2
Tony.Hansen <tony.hansen@pioneer.com>
2018-03-06 10:40:14 -0600

    Updated import logic to search header for names

--- import.py
+++ import.py
@@ -2,5 +2,24 @@ import csv
 
 exImport = open("Data/Inventory/InventoryImportTest.csv")
 exReader = csv.reader(exImport)
+
+# prints the header line of the file
+header = exReader.__next__()
+
+imCount = header.index('Count')
+imName = header.index('Name')
+imEdition = header.index('Edition')
+imFoil = header.index('Foil')
+
+
+# iterates through subsequent rows of data
+def checkfoil(param):
+    if param == "Yes":
+        result = "Foil"
+    else:
+        result = "Normal"
+    return result
+
+
 for row in exReader:
-    print(str(row))
+    print(", ".join([row[imCount], row[imName], row[imEdition], checkfoil(row[imFoil])]))
